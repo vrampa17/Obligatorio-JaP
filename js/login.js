bdeb8a44b9c87799ae5d0176b282cbff505b0cea
vrampa17 <victoriarampasaldivia@gmail.com>
2022-08-16 21:17:17 -0300

    Agrego validación con boostrap
    Agrego rojo si esta vacío, para que se parezca mas a la imagen

--- js/login.js
+++ js/login.js
@@ -2,9 +2,8 @@ document.getElementById("logBtn").addEventListener("click", function () {
     var pass1 = document.getElementById("password");
     var email = document.getElementById("email");
 
-    //debe cumplir con los siquientes requisitos: 
     //los campos de texto no pueden estar vacios
-    if((pass1.value == "")||(email.value == "")){
+    if((pass1.value == "")||(email.value == "")){      
     alert("Completar campos")
     }else{
         window.location.href = "https://vrampa17.github.io/Obligatorio-JaP/inicio";
