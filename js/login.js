89be157dbe6a35725466ff17ac860932374e9411
vrampa17 <victoriarampasaldivia@gmail.com>
2022-08-21 11:00:01 -0300

    Cambios de redireccion al inicio y no al login, creo la funcion mostrar autos

--- js/login.js
+++ js/login.js
@@ -3,9 +3,9 @@ document.getElementById("logBtn").addEventListener("click", function () {
     var email = document.getElementById("email");
 
     //los campos de texto no pueden estar vacios
-    if((pass1.value == "")||(email.value == "")){      
-    alert("Completar campos")
-    }else{
+    if ((pass1.value == "") || (email.value == "")) {
+        alert("Completar campos")
+    } else {
         window.location.href = "https://vrampa17.github.io/Obligatorio-JaP/inicio";
     }
 }
--- js/products.js
+++ js/products.js
@@ -1,40 +1,40 @@
 let listado = []
 
 
-document.addEventListener("DOMContentLoaded", function(e){
- 
-    getJSONData(PODUCTS_AUTOS_URL).then(function(resultObj){
-        if (resultObj.status == "ok"){
-            listado = resultObj.data
+function mostrarAutos(){
+    let htmlContentToAppend = "";
 
-                let htmlContentToAppend = "";
-        
-                 for (let autos of listado.products){
-            
-                        htmlContentToAppend += `
-                        <div onclick="setCatID(${autos.id})" class="list-group-item list-group-item-action cursor-active">
-                        <div class="row">
-                            <div class="col-3">
-                                <img src="${autos.image}" alt="${autos.description}" class="img-thumbnail">
-                            </div>
-                            <div class="col">
-                                <div class="d-flex w-100 justify-content-between">
-                                    <h4 class="mb-1">${autos.name} - ${autos.currency} ${autos.cost}</h4>
-                                    <small class="text-muted">${autos.soldCount} vendidos</small>
-                                </div>
-                                <p class="mb-1">${autos.description}</p>
-                            </div>
+    for (let autos of listado.products) {
+
+        htmlContentToAppend += `
+                <div onclick="setCatID(${autos.id})" class="list-group-item list-group-item-action cursor-active">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="${autos.image}" alt="${autos.description}" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">${autos.name} - ${autos.currency} ${autos.cost}</h4>
+                            <small class="text-muted">${autos.soldCount} vendidos</small>
                         </div>
-                        </div>
-                        `
-                }
-                document.getElementById("prueba").innerHTML = htmlContentToAppend;
-        
+                        <p class="mb-1">${autos.description}</p>
+                    </div>
+                </div>
+                </div>
+                `
+    }
+    document.getElementById("autos").innerHTML = htmlContentToAppend;
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+
+    getJSONData(PODUCTS_AUTOS_URL).then(function (resultObj) {
+        if (resultObj.status == "ok") {
+            listado = resultObj.data
+            mostrarAutos()
         }
     })
-} )
-
-
+})
 
 
 
